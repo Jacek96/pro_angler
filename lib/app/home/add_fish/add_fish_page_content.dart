c5e6497a5e53e008c35f57eb333e532272da9e02
Jacek Ferdynus <jacekferdynus96@gmail.com>
2023-06-27 19:53:47 +0200

    Implement adding fishes

--- lib/app/home/add_fish/add_fish_page_content.dart
+++ lib/app/home/add_fish/add_fish_page_content.dart
@@ -1,14 +1,55 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class AddFishPageContent extends StatelessWidget {
+class AddFishPageContent extends StatefulWidget {
   const AddFishPageContent({
     super.key,
   });
 
   @override
+  State<AddFishPageContent> createState() => _AddFishPageContentState();
+}
+
+class _AddFishPageContentState extends State<AddFishPageContent> {
+  var fishName = '';
+  var fishSize = '';
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Dodaj rybę'),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            decoration: const InputDecoration(
+              hintText: 'Podaj gatunek ryby',
+            ),
+            onChanged: (newValue) {
+              setState(() {
+                fishName = newValue;
+              });
+            },
+          ),
+          TextField(
+            decoration: const InputDecoration(
+              hintText: 'Podaj wymiar ryby',
+            ),
+            onChanged: (newValue) {
+              setState(() {
+                fishSize = newValue;
+              });
+            },
+          ),
+          ElevatedButton(
+            onPressed: () {
+              FirebaseFirestore.instance.collection('fishes').add(
+                {'name': fishName, 'size': fishSize, 'data': '30.03.2023'},
+              );
+            },
+            child: const Text('Dodaj'),
+          ),
+        ],
+      ),
     );
   }
 }
