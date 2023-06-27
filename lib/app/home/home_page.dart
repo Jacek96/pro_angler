69e8087a1f20f4c888e6aba9f3074584d32de0df
Jacek Ferdynus <jacekferdynus96@gmail.com>
2023-06-27 19:34:44 +0200

    Organizing widgets into separate files

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,6 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:pro_angler/app/home/add_fish/add_fish_page_content.dart';
+import 'package:pro_angler/app/home/fishes/fishes_page_content.dart';
+import 'package:pro_angler/app/home/my_account/my_account_page_content.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({
@@ -73,100 +75,6 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-    );
-  }
-}
-
-class MyAccountPageContent extends StatelessWidget {
-  const MyAccountPageContent({
-    super.key,
-    required this.email,
-  });
-
-  final String? email;
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("Jesteś zalogowany jako $email"),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-            child: const Text('Wyloguj'),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class AddFishPageContent extends StatelessWidget {
-  const AddFishPageContent({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Dodaj rybę'),
-    );
-  }
-}
-
-class FishesPageContent extends StatelessWidget {
-  const FishesPageContent({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-        stream: FirebaseFirestore.instance.collection('fishes').snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return const Center(child: Text('Coś poszło nie tak'));
-          }
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: Text('Ładowanie'));
-          }
-
-          final documents = snapshot.data!.docs;
-
-          return ListView(
-            children: [
-              for (final document in documents) ...[
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(document['name']),
-                      Text(document['size'].toString()),
-                      Text(document['data']),
-                    ],
-                  ),
-                ),
-              ],
-            ],
-          );
-        });
-  }
-}
-
-class WeatherPageContent extends StatelessWidget {
-  const WeatherPageContent({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Pogoda'),
     );
   }
 }
